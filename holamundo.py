45dd9a90851727cca69e351cfaa8778d77a0421b
MoctezumaEspiniza <espinozaromeromoctezuma@gmail.com>
2022-06-03 02:24:10 -0500

    first commit

---
+++ holamundo.py
@@ -0,0 +1,5 @@
+nombre = input("como te llamas? ")
+
+print(f"Hola, {nombre}")#se puede poner asi para que comprenda la variavle
+
+print("Hola, " + nombre)#o asi en caso de que la varialbe sea una palabra normal 
